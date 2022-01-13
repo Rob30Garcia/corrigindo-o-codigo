36af3310a3ae2d2921f513260fc0f75f701e8592
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-01-13 18:48:18 -0400

    chore: fix delete

--- src/index.js
+++ src/index.js
@@ -52,11 +52,11 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  repositories.splice(repositoryIndex, 1);
+  repositories.splice(repositories[repositoryIndex], 1);
 
   return response.status(204).send();
 });
